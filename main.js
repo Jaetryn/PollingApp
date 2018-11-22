df969783e4516d8c04801cf04e0897437a9f22fd
John <uhohthepopo@gmail.com>
2018-11-22 02:43:36 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = main;
+
+function main(){
+    document.getElementById("planEvent").addEventListener("click", switchPagePlan);
+    document.getElementById("findEvent").addEventListener("click", switchPageFind);
+}
+
+function switchPagePlan(){
+    window.location.href = "plan/plan.html";
+}
+
+function switchPageFind(){
+    window.location.href = "find/find.html";
+}
